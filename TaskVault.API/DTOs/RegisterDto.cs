390b1fac70497cec48bdc3b26d2f245594ccf49d
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-15 16:25:58 -0400

    feat(dtos): add Username with length validation to RegisterDto

--- TaskVault.API/DTOs/RegisterDto.cs
+++ TaskVault.API/DTOs/RegisterDto.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskVault.API.DTOs;
 
 public class RegisterDto
 {
+    
+    [Required(ErrorMessage = "Username is required")]
+    [MinLength (3, ErrorMessage = "Username must be atleast 3 characters")]
+    [MaxLength (50, ErrorMessage = "Username cannot exceed 50 characters")]
+
+    public string Username { get; set; } = string.Empty;
 
 }
