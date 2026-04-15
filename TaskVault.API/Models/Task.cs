58ac22b45c0f3f117abdf885ca38d15154f9524c
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-15 16:04:03 -0400

    feat(models): add Title and nullable Description to Task model

--- TaskVault.API/Models/Task.cs
+++ TaskVault.API/Models/Task.cs
@@ -8,4 +8,7 @@ public class Task
     public int Id { get; set; }
     public int UserId { get; set; }
 
+    public string Ttile { get; set; } = string.Empty;
+    public string? Description { get; set; }
+
 }
