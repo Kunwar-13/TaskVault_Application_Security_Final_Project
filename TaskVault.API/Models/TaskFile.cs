e845ca8966dcbff3edaabf65ae230faa350d44f9
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-15 16:12:28 -0400

    feat(models): add OriginalName, StoredName, FileSize, and MimeType to TaskFile model

--- TaskVault.API/Models/TaskFile.cs
+++ TaskVault.API/Models/TaskFile.cs
@@ -7,4 +7,9 @@ public class TaskFile
     public int Id { get; set; }
     public int TaskId { get; set; }
 
+    public string OriginalName { get; set; } = string.Empty;
+    public string StoredName {  get; set; } = string.Empty;
+    public int FileSize { get; set; }
+    public string MimeType { get; set; } = string.Empty;
+
 }
