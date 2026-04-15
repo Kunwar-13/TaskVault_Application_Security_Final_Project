80821a8085af19797245ee71532e28ba3f8bdfd8
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-15 15:57:14 -0400

    feat(models): add Username, Email, and PasswordHash to User model

--- TaskVault.API/Models/User.cs
+++ TaskVault.API/Models/User.cs
@@ -8,4 +8,8 @@ public class User
     public bool IsActive { get; set; }
     public DateTime CreatedAt { get; set; }
 
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
 }
