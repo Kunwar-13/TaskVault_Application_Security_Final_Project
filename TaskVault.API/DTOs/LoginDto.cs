f0744759406af26374368a389279d1520580e4cc
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-15 22:23:19 -0400

    feat(dtos): add Password with max length validation to LoginDto

--- TaskVault.API/DTOs/LoginDto.cs
+++ TaskVault.API/DTOs/LoginDto.cs
@@ -10,4 +10,8 @@ public class LoginDto
     [EmailAddress(ErrorMessage = "Invalid email format")]
     public string Email { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Password is required")]
+    [MaxLength(100, ErrorMessage = "Password cannot exceed 100 characcters")]
+    public string Password { get; set; } = string.Empty;
+
 }
