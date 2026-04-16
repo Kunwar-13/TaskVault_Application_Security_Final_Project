7b23560bab132a4d1cd6d0710f3c23182e028c90
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-15 22:21:06 -0400

    feat(dtos): add Email with format validation to LoginDto

--- TaskVault.API/DTOs/LoginDto.cs
+++ TaskVault.API/DTOs/LoginDto.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskVault.API.DTOs;
 
 public class LoginDto
 {
+    
+    [Required(ErrorMessage = "Email is Required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    public string Email { get; set; } = string.Empty;
 
 }
