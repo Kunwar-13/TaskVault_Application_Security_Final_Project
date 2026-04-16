cd98a10ad43baf2113e84f7a3de9cb9c01fe6b92
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-15 23:52:20 -0400

    feat(auth): scaffold AuthController with ApiController attribute and base route

---
+++ TaskVault.API/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskVault.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+
+}
