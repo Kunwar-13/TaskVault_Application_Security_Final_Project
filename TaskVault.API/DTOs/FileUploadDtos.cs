7b71f64520952e331eb725e4c225180fb165228d
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-15 22:30:27 -0400

    feat(dtos): add required IFormFile property to FileUploadDto

--- TaskVault.API/DTOs/FileUploadDtos.cs
+++ TaskVault.API/DTOs/FileUploadDtos.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace TaskVault.API.DTOs;
 
 public class FileUploadDto
 {
+    [Required(ErrorMessage = "File is required")]
+    public IFormFile File { get; set; } = null!;
+
 
 }
