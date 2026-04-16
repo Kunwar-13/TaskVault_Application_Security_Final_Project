77a772967bf74f3ca1cf342a2a6acc3c1b0a0aa4
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-16 00:45:10 -0400

    feat(tasks): scaffold TaskController with Authorize at class level

---
+++ TaskVault.API/Controllers/TaskController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskVault.API.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/[controller]")]
+public class TaskController : ControllerBase
+{
+
+}
