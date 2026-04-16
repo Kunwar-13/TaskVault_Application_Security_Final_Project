c52ddd97ba7aeb402361e096b2d19974e7b6865c
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-15 23:38:56 -0400

    feat(auth): add constructor injecting DbContext and IConfiguration into AuthService

--- TaskVault.API/Services/AuthServices.cs
+++ TaskVault.API/Services/AuthServices.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using TaskVault.API.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace TaskVault.API.Services;
 
@@ -9,5 +11,16 @@ public interface IAuthService
 
 public class AuthService : IAuthService
 {
+   
+    private readonly DbContext _db;
+    private readonly IConfiguration _config;
+
+    public AuthService(DbContext db, IConfiguration config)
+    {
+    
+        _db = db;
+        _config = config;
+    
+    }
 
 }
