21bfa0213515eb185e059dcf6fc80e0e7c6c47b7
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-16 00:26:19 -0400

    feat(tasks): add constructor injecting DbContext into TaskService

--- TaskVault.API/Services/TaskService.cs
+++ TaskVault.API/Services/TaskService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TaskVault.API.Data;
 
 namespace TaskVault.API.Services;
 
@@ -9,5 +10,13 @@ public interface ITaskService
 
 public class TaskService : ITaskService
 {
+    private readonly DbContext _db;
+
+    public TaskService(DbContext db)
+    {
+     
+        _db = db;
+    
+    }
 
 }
