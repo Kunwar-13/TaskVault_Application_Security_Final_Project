4d54fe28f95adc7e4e2e4faaee758cf96acf3855
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-16 00:45:58 -0400

    feat(tasks): add constructor injecting ITaskService into TaskController

--- TaskVault.API/Controllers/TaskController.cs
+++ TaskVault.API/Controllers/TaskController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TaskVault.API.Services;
 
 namespace TaskVault.API.Controllers;
 
@@ -9,5 +10,14 @@ namespace TaskVault.API.Controllers;
 [Route("api/[controller]")]
 public class TaskController : ControllerBase
 {
+
+    private readonly ITaskService _taskService;
+
+    public TaskController(ITaskService taskService)
+    {
+     
+        _taskService = taskService;
+    
+    }
 
 }
