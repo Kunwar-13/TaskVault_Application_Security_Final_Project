4203f534654f3aabd57d3cd6213eaa710a488577
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2026-04-16 00:36:57 -0400

    feat(helpers): add GetUserId extension method on ClaimsPrincipal for controller use

---
+++ TaskVault.API/Helpers/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace TaskVault.API.Helpers;
+
+public static class ClaimsPrincipalExtensions
+{
+   
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+     
+        var claim = user.FindFirst(ClaimTypes.NameIdentifier)
+                 ?? user.FindFirst("sub")
+                 ?? throw new UnauthorizedAccessException("User ID claim not found.");
+
+        return int.Parse(claim.Value);
+    
+    }
+
+}
